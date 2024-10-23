11f2f3edb49abab7961662628d4e45dfe81d5b76
Rahulsahu7389 <rahulsahuppd@gmail.com>
2024-10-23 10:02:13 +0530

    stored database

--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render,HttpResponse
+from home.models import Contact
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        name1 = request.POST.get('name')#ye vo wala hai yo post request se milega
+        password = request.POST.get('password')
+        contact = Contact(name = name1 , password = password)
+        # messages.success(request, 'your login was successful')
+        contact.save()#saves table
+
     return render(request,'index.html')
 
 def about(request):
